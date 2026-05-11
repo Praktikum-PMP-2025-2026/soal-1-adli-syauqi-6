68043e560bf7af1f38f47724c5429740b05f3c16
Muhammad Adli Syauqi <121152721+adli-syauqi@users.noreply.github.com>
2026-05-11 08:29:40 +0700

    solve
    Implement graph representation and degree calculation.

--- soal1.c
+++ soal1.c
@@ -1,5 +1,69 @@
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2025/2026
+ *   Modul               : Foundation of Algorithm
+ *   Hari dan Tanggal    : 11 Mei 2026
+ *   Nama (NIM)          : Muhammad Adli Syauqi (13224082)
+ *   Nama File           : soal1.c
+ *   Deskripsi           : Program memberikan graf dalam format adjacency matrix, kemudian mengembalikan degree setiap vertex, vertex dengan degree maksimum, dan vertex yang isolated
+ *  
+ */
+
+
 #include <stdio.h>
 
-int main(void) {
-    return 0;
+#define MAX 100
+
+typedef struct{
+    int size;
+    int data[MAX][MAX];
+} Graph;
+
+int main(){
+    int n;
+    scanf("%d", &n);
+
+    Graph graph;
+    graph.size = n;
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            scanf("%d", &graph.data[i][j]);
+            // printf("%d ", graph.data[i][j]);
+        }
+    }
+
+    int degree = 0;
+    int max_vertex_index = 0;
+    int max_vertex_degree = 0;
+
+    int isolated_index = -5;
+
+    for(int i = 0; i < n; i++){
+        degree = 0;
+        for(int j = 0; j < n; j++){
+            
+            if(graph.data[i][j] == 1){
+                degree++;
+            }
+        }
+
+        if(max_vertex_degree < degree){
+            max_vertex_index = i;
+            max_vertex_degree = degree;
+        }
+
+        if(degree == 0){
+            isolated_index = i;
+        }
+
+        printf("DEGREE %d %d\n", i, degree);
+    }
+
+    printf("MAX_VERTEX %d\n", max_vertex_index);
+
+    if(isolated_index >= 0){
+        printf("ISOLATED %d", isolated_index);
+    } else{
+        printf("ISOLATED NONE");
+    }
+
 }
